87432395bc553ce19014c23227e69c761de542da
Aditya Seth <adityaseth.cse@gmail.com>
2023-03-01 11:53:19 +0530

    niceee

---
+++ Problem-of-the-day/25.02.2023/Solution_Adi.js
@@ -0,0 +1,18 @@
+function maxProfit(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+      if (prices[i] < minPrice) {
+        minPrice = prices[i];
+      } else if (prices[i] - minPrice > maxProfit) {
+        maxProfit = prices[i] - minPrice;
+      }
+    }
+    
+    return maxProfit;
+  }
+  
+  // Example usage
+  const prices = [7, 1, 5, 3, 6, 4];
+  console.log(maxProfit(prices)); // expected output: 5
